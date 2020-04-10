899a4eae20c183f656868420b70622669f3ad0e0
srz_zumix <zumix.cpp@gmail.com>
2020-04-10 09:46:04 +0900

    fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,22 +21,22 @@ test() {
     PIXEL=$(( $W * $H ))
 
     if [[ ! " ${FORMATS[@]} " =~ " ${FORMAT} " ]]; then
-        cho "::error:: $1: format $FORMAT, required $INPUT_FORMATS." 
+        echo "::error:: $1: format $FORMAT, required $INPUT_FORMATS." 
         export RESULT=1
     fi
     if [ $X_DPI != ${INPUT_DPI} ]; then
-        cho "::error:: $1: x dpi $X_DPI, required $INPUT_DPI." 
+        echo "::error:: $1: x dpi $X_DPI, required $INPUT_DPI." 
         export RESULT=1
     fi
     if [ $Y_DPI != ${INPUT_DPI} ]; then
-        cho "::error:: $1: y dpi $Y_DPI, required $INPUT_DPI." 
+        echo "::error:: $1: y dpi $Y_DPI, required $INPUT_DPI." 
         export RESULT=1
     fi
     if [ $PIXEL != ${INPUT_PIXEL_LIMIT} ]; then
-        cho "::error:: $1: pixel $PIXEL, required $INPUT_PIXEL_LIMIT." 
+        echo "::error:: $1: pixel $PIXEL, required $INPUT_PIXEL_LIMIT." 
         export RESULT=1
     fi
-} 
+}
 
 for f in $(find . -type f); do
     test $f
